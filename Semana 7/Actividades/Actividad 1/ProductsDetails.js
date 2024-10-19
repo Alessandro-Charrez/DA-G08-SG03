9982e54658036d7bb2a899e8d12f48a49ce4aab5
Alessandro-Charrez <alessandro.charrez@ucsm.edu.pe>
2024-10-19 00:27:38 -0500

    ProductsDetails.js

---
+++ Semana 7/Actividades/Actividad 1/ProductsDetails.js
@@ -0,0 +1,10 @@
+import { useParams } from "react-router-dom"; 
+export const ProductDetails = () => { 
+const { productName } = useParams(); 
+return ( 
+ 	<div> 
+ 				<h3>Details of product: {productName}</h3> 
+ 				<p>This is a great {productName}!</p> 
+ 			</div> 
+ 		); 
+}; 
